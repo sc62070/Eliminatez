bbf1ab7be3a67e8f281d419f7351e9dae7791879
Hyuk Jin Choi <sc62070@gmail.com>
2022-04-19 15:35:31 +0900

    UI 수정

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -13,7 +13,7 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         isGameOver = false;
-        playerHP = 100;
+        playerHP = 1000000;
     }
 
     // Update is called once per frame
